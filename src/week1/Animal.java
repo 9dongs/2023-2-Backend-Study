c30dba72bb9a1ca24396e74c4f1c89c28b2d8e55
9dongs <ehdgusqaz98@gmail.com>
2023-09-20 00:22:37 +0900

    Week1 과제 제출

---
+++ src/week1/Animal.java
@@ -0,0 +1,20 @@
+package week1;
+
+public abstract class Animal{
+    public static void main(String[] args) {
+        // Animal a = new Animal(); // 추상 클래스는 인스턴스를 생성할 수 없음.
+        Cat c = new Cat();
+        Dog d = new Dog();
+
+        c.speak();
+        d.speak();
+    }
+    public abstract void speak();
+}
+class Cat extends Animal { public void speak() {
+    System.out.println("cat says Meow!");
+} }
+class Dog extends Animal { public void speak() {
+    System.out.println("dog says Woof!");
+} }
+
---
+++ src/week1/Calculator.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class Calculator {
+    int plus(int a, int b){
+        return a+b;
+    } // a + b 값 반환
+    int minus(int a, int b){
+        return a-b;
+    } // a - b 값 반환
+    int mul(int a, int b){
+        return a*b;
+    } // a * b 값 반환
+    int div(int a, int b){
+        return a/b;
+    } // a / b 값 반환
+}
